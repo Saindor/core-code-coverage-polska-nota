35b3f7702bafa7c552cb76b4d0a474e5a3c17d05
Saindor <msardor088@gmail.com>
2025-12-29 01:06:13 +0500

    Add tests for Reverse Polish Notation Calculator

--- test/ru/yandex/javacourse/ReversePolishNotationCalculatorTest.java
+++ test/ru/yandex/javacourse/ReversePolishNotationCalculatorTest.java
@@ -1,8 +1,5 @@
 package ru.yandex.javacourse;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,10 +9,27 @@ public class ReversePolishNotationCalculatorTest {
 
     @Test
     public void shouldCalculateAddition() {
-        //TODO
+        assertEquals(10, rpn.calculatePolishNotation("6 4 +"));
     }
 
-    //TODO
+    @Test
+    public void shouldCalculateSubtraction() {
+        assertEquals(6, rpn.calculatePolishNotation("10 4 -"));
+    }
 
+    @Test
+    public void shouldCalculateMultiplication() {
+        assertEquals(25, rpn.calculatePolishNotation("5 5 *"));
+    }
+
+    @Test
+    public void shouldWorkWithNegativeNumbers() {
+        assertEquals(1, rpn.calculatePolishNotation("-2 3 +"));
+    }
+
+    @Test
+    public void shouldIgnoreExtraSpaces() {
+        assertEquals(7, rpn.calculatePolishNotation("   4    3   +   "));
+    }
 }
 
